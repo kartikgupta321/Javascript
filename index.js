72f8e672d60f08247dd3a768ecf2ab3456776d3f
Kartik <kartik.gupta2505@gmail.com>
2021-08-10 20:50:22 +0530

    10 august

---
+++ index.js
@@ -0,0 +1,13 @@
+// 1. ways to write 
+// alert("oye kya krra h");
+// document.write("this is document write");
+
+// 2. console api 
+console.log("hello world555", 9 ** 9);
+// console.warn("this is a warning");
+// console.error("this is a error");
+
+// 3. variables
+var no1=50;
+var no2=133;
+console.log(no1+no2);
