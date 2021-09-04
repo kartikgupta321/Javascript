58038ec21e5daa886d8f93442bcceb649e03cef6
Kartik <kartik.gupta2505@gmail.com>
2021-09-04 23:44:51 +0530

    4sept

--- index.js
+++ index.js
@@ -8,6 +8,77 @@ console.log("hello world555", 9 ** 9);
 // console.error("this is a error");
 
 // 3. variables
-var no1=50;
-var no2=133;
-console.log(no1+no2);
+// var no1=50;
+// var no2=133;
+// console.log(no1+no2);
+
+// 4. data types 
+// var str="this is a string";
+// var marks="kartik: 100, gupta:99,anurag:89";
+// var a =true;
+// var undefined;
+// var n = null;
+// var arr=[1,2,3,4,50];
+// console.log(arr);
+
+// 5.operators
+// console.log(arr[0]<n);
+// console.log(true||false);
+// console.log(!true);
+
+// 6.functions
+// function avg(a,b){
+//     return (a+b)/2;
+// }
+// console.log(avg(5,10));
+
+// 7.conditonals, loop
+// if(){
+// }
+// else if(){
+// }
+// arr=["dejfbdf",5,73,5,35,4];
+// console.log(arr);
+// for (var i= 0; i< arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// arr.forEach(function(element) {
+// console.log(element);  
+// })
+// let j =0;
+// console.log(arr.length);
+// while (j < arr.length) {
+//     console.log(arr[j]);
+//     j++;
+// }
+// console.log(j);
+// do(){
+// }while()
+
+// 8.array methods
+
+// break,continue;
+
+// console.log(arr.length);
+// arr.pop();
+// arr.push("lll");
+// arr.unshift("kyu");
+// console.log(arr);
+
+// 9.string methods
+// let string='i want to play football';
+// console.log(string.length);
+// console.log(string.indexOf("a"));
+// console.log(string.lastIndexOf("a"));
+// console.log(string.slice(0,9));
+// d=string.replace("i","you");
+// console.log(d);
+
+// 10.dates
+// let mydate=new Date();
+// console.log(mydate);
+// console.log(mydate.getTime());
+// console.log(mydate.getFullYear());
+// console.log(mydate.getDay());
+
+// 11.dom manipulation
---
+++ neog.js
@@ -0,0 +1,30 @@
+// 1.
+// console.log("kartik gupta");
+// 2.
+var readlinesync =require('readline-sync');
+// var username = readlinesync.question('what is your name- ');
+// console.log(username);
+// 3.
+// var one= 'hello ';
+// var two = 'welcome';
+// console.log(one+two);
+// 4.
+// console.log('welcome '+username);
+// 5.
+// var age = readlinesync.question('what is my age- ');
+// if (age==20) 
+//     console.log('true');
+// else
+//     console.log('false');
+// 6.
+// let score=0;
+// var ans = readlinesync.question('where do i live- ');
+// if(ans == 'dehradun')
+//     score++;
+// console.log(score);
+// 7.
+// function add(no1,no2){
+//     return no1+no2;
+// }
+// console.log(add(4,5));
+// 8.
